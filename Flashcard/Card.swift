9c49e6bf9623bb64ea2dcb3c25412520d598ba3f
Ipsky <yap@usc.edu>
2020-07-19 15:50:29 -0700

    add json parsing

--- Flashcard/Card.swift
+++ Flashcard/Card.swift
@@ -6,10 +6,29 @@
 //
 
 import Foundation
-struct Card: Identifiable, Equatable {
+
+fileprivate struct RawCard: Decodable {
+    var prompt: String
+    var answer: String
+}
+
+struct Card: Identifiable, Equatable, Decodable {
     var id = UUID()
     var prompt: String
     var answer: String
     
     static let example = Card (prompt: "Prompt", answer: "Answer")
+    
+    init(prompt: String, answer: String) {
+        id = UUID()
+        self.prompt = prompt
+        self.answer = answer
+    }
+    
+    init(from decoder: Decoder) throws {
+        let rawCard = try RawCard(from: decoder)
+        id = UUID()
+        prompt = rawCard.prompt
+        answer = rawCard.answer
+    }
 }
