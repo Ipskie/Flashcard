36596a789800eac680fb3743a40ccba6eb602fc5
Ipsky <yap@usc.edu>
2020-07-19 15:25:18 -0700

    visual fix

--- Flashcard/CardView.swift
+++ Flashcard/CardView.swift
@@ -11,50 +11,72 @@ struct CardView: View {
     var card: Card
     @State var showAnswer = false
     @State private var offset = CGSize.zero
-    @State var rotation = Angle.zero
     
+    /// magnifies the movement of the user's swipe, so small gestures can dismiss the view
     private let scaling = CGFloat(3)
     var removal: ((Card, Bool) -> Void)? = nil
     
     var body: some View {
         GeometryReader { geo in
-            HStack(spacing: .zero) {
-                Text(card.prompt)
-                    .frame(width: geo.size.width / 2)
-                    .font(.title)
-                Divider()
-                    .padding([.top, .bottom])
-                Text(card.answer)
-                    .frame(width: geo.size.width / 2)
-                    .blur(radius: showAnswer ? .zero : 10)
-                    .font(.title)
+            if geo.size.width > geo.size.height {
+                HStack(spacing: .zero) {
+                    Text(card.prompt)
+                        .frame(maxWidth: geo.size.width / 2)
+                        .font(.title)
+                    Divider()
+                        .padding([.top, .bottom])
+                    Text(card.answer)
+                        .frame(maxWidth: geo.size.width / 2)
+                        .blur(radius: showAnswer ? .zero : 10)
+                        .font(.title)
+                }.background(RoundedRectangle(cornerRadius: geo.size.height / 8).fill(Color(UIColor.systemBackground)))
+            } else {
+                VStack(spacing: .zero) {
+                    Text(card.prompt)
+                        .frame(maxHeight: geo.size.height / 2)
+                        .font(.title)
+                    Divider()
+                        .padding([.leading, .trailing])
+                    Text(card.answer)
+                        .frame(maxHeight: geo.size.height / 2)
+                        .blur(radius: showAnswer ? .zero : 10)
+                        .font(.title)
+                }.background(RoundedRectangle(cornerRadius: geo.size.height / 8).fill(Color(UIColor.systemBackground)))
             }
-            .background(
-                RoundedRectangle(cornerRadius: geo.size.height / 8)
-                    .fill(Color(UIColor.systemBackground))
-                )
-            .shadow(radius: 10)
-            .rotationEffect(.degrees(Double(offset.width / scaling)))
-            .offset(x: offset.width * scaling, y: 0)
-            .gesture(
-                DragGesture()
-                    .onChanged { gesture in
-                        offset = gesture.translation
-                    }
-
-                    .onEnded { _ in
-                        if abs(offset.width) > 100 {
-                            removal?(card, offset.width > 0)
-                        } else {
-                            withAnimation{ offset = .zero }   
-                        }
-                    }
-            )
-            .onTapGesture {
-                withAnimation(.linear(duration: 0.1)) { showAnswer.toggle() }
-            }
-            .padding()
         }
+        .shadow(radius: 10)
+        .rotationEffect(.degrees(Double(offset.width / scaling)))
+        .offset(x: offset.width * scaling, y: 0)
+        .gesture(
+            DragGesture()
+                .onChanged { gesture in
+                    offset = gesture.translation
+                }
+                .onEnded { _ in
+                    if abs(offset.width) > 100 {
+                        removal?(card, offset.width > 0)
+                    } else {
+                        withAnimation{ offset = .zero }
+                    }
+                }
+        )
+        .onTapGesture {
+            withAnimation(.linear(duration: 0.1)) { showAnswer.toggle() }
+        }
+        .padding()
     }
     
+    func stack(geo: GeometryProxy) -> some View {
+        Group {
+            Text(card.prompt)
+                .frame(maxWidth: geo.size.width / 2)
+                .font(.title)
+            Divider()
+                .padding(geo.size.width > geo.size.height ? [.top, .bottom] : [.leading, .trailing])
+            Text(card.answer)
+                .frame(maxWidth: geo.size.width / 2)
+                .blur(radius: showAnswer ? .zero : 10)
+                .font(.title)
+        }
+    }
 }
