466a3531637633619f2c53816c47ae0cd7c46dce
Ipsky <yap@usc.edu>
2020-07-19 15:59:47 -0700

    make scaling uniform

--- Flashcard/CardSession.swift
+++ Flashcard/CardSession.swift
@@ -46,8 +46,14 @@ struct CardSession: View {
 
 extension View {
     func stacked(at position: Int, in total: Int) -> some View {
+        var scale = CGFloat.zero
+        switch total - position {
+        case 1: scale = 1 /// current card
+        case 2: scale = 0.8 /// next card
+        default: scale = 0.5 /// all other cards
+        }
         /// add 1 due to zero indexing, so front card is always 100% of size
         return self
-            .scaleEffect(CGFloat(position + 1) / CGFloat(total))
+            .scaleEffect(scale)
     }
 }
