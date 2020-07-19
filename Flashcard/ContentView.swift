74412992aba068e8eeef2896d7577e309b90987e
Ipsky <yap@usc.edu>
2020-07-19 08:43:28 -0700

    Initial Commit

---
+++ Flashcard/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Flashcard
+//
+//  Created by Secret Asian Man Dev on 19/7/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Flashcard/FlashcardApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashcardApp.swift
+//  Flashcard
+//
+//  Created by Secret Asian Man Dev on 19/7/20.
+//
+
+import SwiftUI
+
+@main
+struct FlashcardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
