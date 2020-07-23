72bdcd5426a87472e4a35a864e7317984ba7a23a
Ipsky <yap@usc.edu>
2020-07-23 10:40:29 -0700

    typo, initialize test instead of deck

--- Models/CoreData/Test+CoreDataClass.swift
+++ Models/CoreData/Test+CoreDataClass.swift
@@ -19,7 +19,7 @@ public class Test: NSManagedObject {
     
     /// create a Core Data deck by unpacking it from a bundled JSON file
     init(prompts: [Snippet], answers: [Snippet], context: NSManagedObjectContext) throws {
-        super.init(entity: Deck.entity(), insertInto: context)
+        super.init(entity: Test.entity(), insertInto: context)
         self.prompts = prompts.map{$0.rawValue}
         self.answers = answers.map{$0.rawValue}
     }
