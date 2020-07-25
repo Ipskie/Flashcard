3c3c113a8c180ce16f75c413f484c643b59b43d3
Ipsky <yap@usc.edu>
2020-07-25 15:01:45 -0700

    Update SnippetPicker.swift

--- Views/SnippetPicker.swift
+++ Views/SnippetPicker.swift
@@ -11,39 +11,66 @@ import CoreData
 struct SnippetPicker: View {
     
     @Environment(\.managedObjectContext) var moc: NSManagedObjectContext
-    
+    @State var editMode: EditMode = .inactive
     var deck: Deck
-    @Binding var promptTypes: [Snippet]
-    @Binding var answerTypes: [Snippet]
+    @State var prompts = [Snippet]()
+    @State var answers = [Snippet]()
+    
+    init(deck: Deck) {
+        self.deck = deck
+        
+    }
+    
+    /// store coordinator isn't present on init
+    func onAppear() {
+        precondition(moc.persistentStoreCoordinator != nil)
+        prompts = deck.getTest(moc: moc)!._prompts
+        answers = deck.getTest(moc: moc)!._answers
+    }
     
     var body: some View {
         List {
-//            Section(header: Text("Shown Prompt")) {
-//                Picker("Shown Prompt", selection: $prompt) {
-//                    ForEach(Snippet.allCases, id: \.self) {
-//                        Text($0.name)
-//                    }
-//                }
-//                .pickerStyle(SegmentedPickerStyle())
-//            }
-//            .onChange(of: prompt, perform: {
-//                deck.promptType = $0
-//                try! moc.save()
-//            })
-//            Section(header: Text("Hidden Answer")) {
-//                Picker("Hidden Answer", selection: $answer) {
-//                    ForEach(Snippet.allCases, id: \.self) {
-//                        Text($0.name)
-//                    }
-//                }
-//                .pickerStyle(SegmentedPickerStyle())
-//            }
-//            .onChange(of: answer, perform: {
-//                deck.answerType = $0
-//                try! moc.save()
-//            })
+            EditableList(snippets: $prompts)
         }
-        .listStyle(GroupedListStyle())
+        .onAppear{ onAppear() }
+        .navigationBarItems(trailing: EditButton())
+        .listStyle(InsetGroupedListStyle())
         .navigationTitle("Card Type")
+        .environment(\.editMode, self.$editMode)
+    }
+    
+    func EditableList(snippets: Binding<[Snippet]>) -> some View {
+        Group {
+            Section(header: Text("Card Prompts")) {
+                ForEach(snippets.wrappedValue, id: \.self) { snippet in
+                    Text(snippet.name)
+                }
+                .onMove { source, destination in
+                    snippets.wrappedValue.move(fromOffsets: source, toOffset: destination)
+                }
+                .onDelete { offsets in
+                    snippets.wrappedValue.remove(atOffsets: offsets)
+                }
+            }
+            if editMode == .active {
+                Section (header: Text("Add Prompts")) {
+                    ForEach(Snippet.allCases.filter{!snippets.wrappedValue.contains($0)}, id: \.self) { snippet in
+                        Button {
+                            withAnimation {
+                                snippets.wrappedValue.append(snippet)
+                            }
+                        } label: {
+                            HStack {
+                                Image(systemName: "plus.circle.fill")
+                                    .foregroundColor(.green)
+                                Text(snippet.name)
+                            }
+                        }
+                        .buttonStyle(PlainButtonStyle())
+                    }
+                }
+            }
+
+        }
     }
 }
