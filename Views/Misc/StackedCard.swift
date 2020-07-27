2f2ebd9a8a534404d390ba8adf764cf3759c1615
Ipsky <yap@usc.edu>
2020-07-27 11:22:08 -0700

    break stacked out

---
+++ Views/Misc/StackedCard.swift
@@ -0,0 +1,21 @@
+//
+//  StackedCard.swift
+//  Flashcard
+//
+//  Created by Secret Asian Man Dev on 27/7/20.
+//
+
+import SwiftUI
+extension View {
+    func stacked(at position: Int, in total: Int) -> some View {
+        var scale = CGFloat.zero
+        switch total - position {
+        case 1: scale = 1 /// current card
+        case 2: scale = 0.8 /// next card
+        default: scale = 0.5 /// all other cards
+        }
+        /// add 1 due to zero indexing, so front card is always 100% of size
+        return self
+            .scaleEffect(scale)
+    }
+}
