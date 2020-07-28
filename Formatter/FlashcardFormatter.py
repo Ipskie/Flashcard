7aa8d05a4b514522d102ca93c12c36457b975b71
Ipsky <yap@usc.edu>
2020-07-28 13:39:44 -0700

    get some example kanji XML

--- Formatter/FlashcardFormatter.py
+++ Formatter/FlashcardFormatter.py
@@ -1,5 +1,12 @@
+import xml.etree.ElementTree as ET
 import regex as re
 
+def fileContents(name: str) -> str:
+    f = open(name,"r")
+    return f.read()
 
-f = open("file.txt","r")
-print(f.read())
+
+
+if __name__ == "__main__":
+    tree = ET.parse('file.xml')
+    root = tree.getroot()
---
+++ Formatter/processing.md
@@ -0,0 +1,2 @@
+`<dic_ref.*?>.*?</dic_ref>\n` dictionary numbers
+`<dic_number>\n</dic_number>\n` dictionary section
